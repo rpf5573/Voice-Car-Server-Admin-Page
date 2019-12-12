66e57fa7b59b335371e22769bfb345f8b5f59d60
Yoon <rpf5573@gmail.com>
2019-12-12 10:42:55 +0900

    tt

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -2,9 +2,10 @@ import { Express, RequestHandler } from 'express';
 import QueryHub from '../query';
 import constants from '../utils/constants';
 
-console.log("여기도 읽히긴 혔다잉");
-
 export default (app:Express, QH: QueryHub) => {
+  
+  console.log("여기도 읽히긴 혔다잉");
+
   app.get('/test', async (req, res) => {
     return res.status(201).json({error: "테스트 API 호출 in userRouter.ts"});
   });
