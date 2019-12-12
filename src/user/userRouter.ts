86b6df2db828cbc3b9aad7e3726a6e6d9dbcea2c
Yoon <rpf5573@gmail.com>
2019-12-12 10:56:43 +0900

    yeap

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -3,13 +3,8 @@ import QueryHub from '../query';
 import constants from '../utils/constants';
 
 export default (app:Express, QH: QueryHub) => {
-  sdfsdlfnsldf
-  app.post('/test', async (req, res) => {
-    return res.status(201).json({error: "테스트 API 호출 in userRouter.ts"});
-  });
   app.post('/user/login', async (req, res) => {
     const pw = req.body.password;
-    console.log("사용자로부터 받은 pw", pw);
     let team = null;
     try {
       let teamPasswords = await QH.teamPasswords.getAll();
