09fdd328dfaae669d8fec5014a94e862419fe830
Yoon <rpf5573@gmail.com>
2019-12-12 11:20:36 +0900

    Fixed

--- src/admin/admin-back/admin-router.ts
+++ src/admin/admin-back/admin-router.ts
@@ -17,7 +17,7 @@ export default (app: Express, uploadHandler: RequestHandler, QH: QueryHub) => {
       srcPath.js = 'admin/bundle.js';
     }
     let initialSettings = await QH.getInitialState();
-    let document = template(initialSettings, srcPath, '', process.env.__group__);
+    let document = template(initialSettings, srcPath, '', process.env.group);
     return res.set('Content-Type', 'text/html').end(document);
   });
 
